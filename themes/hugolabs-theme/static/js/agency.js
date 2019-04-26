3f8c185272e061e3ca45008e592cd3786a3e833d
Nickolas Simard <Nicko@Nickolass-iMac.local>
2019-04-25 20:11:17 -0400

    Re-adding custom validation

--- themes/hugolabs-theme/static/js/agency.js
+++ themes/hugolabs-theme/static/js/agency.js
@@ -62,9 +62,26 @@ $('form[id=contactForm]').submit(function(){
 });
 
 // Contact form validation
-// $.validate({
-//   modules : 'html5, toggleDisabled'
-// });
+$.validate({
+  modules : 'html5, toggleDisabled',
+  
+    onError : function($form) {
+      alert('Validation of form '+$form.attr('id')+' failed!');
+    },
+    onSuccess : function($form) {
+      alert('The form '+$form.attr('id')+' is valid!');
+      return false; // Will stop the submission of the form
+    },
+    onValidate : function($form) {
+      return {
+        element : $('#some-input'),
+        message : 'This input has an invalid value for some reason'
+      }
+    },
+    onElementValidate : function(valid, $el, $form, errorMess) {
+      console.log('Input ' +$el.attr('name')+ ' is ' + ( valid ? 'VALID':'NOT VALID') );
+    }
+});
 //
 // function onContactCaptcha($form) {
 //   $('form#contactForm').submit();
