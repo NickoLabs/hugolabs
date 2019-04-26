366393af5902a138f7771563b6ee97c56d536f53
Nickolas Simard <Nicko@Nickolass-iMac.local>
2019-04-25 21:08:00 -0400

    Remove logging of validation

--- themes/hugolabs-theme/static/js/agency.js
+++ themes/hugolabs-theme/static/js/agency.js
@@ -63,33 +63,7 @@ $('form[id=contactForm]').submit(function(){
 
 // Contact form validation
 $.validate({
-  modules : 'html5',
-
-    onError : function($form) {
-      alert('Validation of form '+$form.attr('id')+' failed!');
-    },
-    onSuccess : function($form) {
-      alert('The form '+$form.attr('id')+' is valid!');
-      return false; // Will stop the submission of the form
-    },
-    onValidate : function($form) {
-      return {
-        element : $('#some-input'),
-        message : 'This input has an invalid value for some reason'
-      }
-    },
-    onElementValidate : function(valid, $el, $form, errorMess) {
-      console.log('Input ' +$el.attr('name')+ ' is ' + ( valid ? 'VALID':'NOT VALID') );
-    }
-});
-
-$('input')
-.on('beforeValidation', function(value, lang, config) {
-  console.log('Input "'+this.name+'" is about to become validated');
-  // Call $(this).attr('data-validation-skipped', 1); to prevent validation
-})
-.on('validation', function(evt, valid) {
-  console.log('Input "'+this.name+'" is ' + (valid ? 'VALID' : 'NOT VALID'));
+  modules : 'html5'
 });
 //
 // function onContactCaptcha($form) {
